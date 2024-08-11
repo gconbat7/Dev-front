0e73aaa40007f6db47cae8dc3a3f25b2f2ab9729
gconbat7 <gildocode7@gmail.com>
2024-08-11 20:29:19 -0300

    Front end do Dev-api

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login"
+import ListarUsuarios from "./pages/Lista";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <h1 className="text=2xl font-pbold text-center">Sistema de Cadastro de Usuários</h1>
+    </header>
+      <Routes>
+        <Route path="/" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/listar-usuarios" element={<ListarUsuarios />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Lista/index.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+function ListarUsuarios() {
+const [allUsers, setAllUsers] = useState()
+
+  useEffect(() => {
+    async function loadUsers() {
+      const token = localStorage.getItem("token");
+      const {
+        data: { users },
+      } = await api.get("/listar-usuarios", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setAllUsers(users)
+    }
+
+    loadUsers();
+  }, []);
+
+  return (
+    <div className="max-w-2xl mx-auto mt-10 bg-white p8 border border-gray-300 rounded-md shadow-lg">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Lista de Usuários</h2>
+
+      <ul className="space-y-2">
+        
+        {allUsers && allUsers.length > 0 && allUsers.map((user) => (
+            <li key={user.id} className="bg-gray-400 p-4 rounded-md">
+                <p className="font-semibold">ID:{user.id}</p>
+                <p className="font-semibold">Nome:{user.name}</p>
+                <p className="font-semibold">Email:{user.email}</p>
+            </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default ListarUsuarios;
